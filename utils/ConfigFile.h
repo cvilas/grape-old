b8a199fde1ecae3bbaa490231a6e5996e96f70ec
Vilas Kumar Chitrakaran <cvilas@gmail.com>
2013-06-21 16:15:06 +0100

    configfile class wip

--- utils/ConfigFile.h
+++ utils/ConfigFile.h
@@ -14,10 +14,43 @@ namespace Grape
 {
 
 /// \brief configuration file reader/writer
-class ConfigFile
+///
+/// ConfigFile class provides methods to read a configuration file containing
+/// named sections with key-value pairs and optional comments. The format
+/// for a configuration entry is as follows:
+/// \code
+/// [name] = [value] [#comment]
+/// \endcode
+///
+/// Multiple nested subsections are supported. Sub-sections are contained
+/// in <name></name> blocks where 'name' identifies the sub-section.
+///
+/// The following is an example configuration file:
+///
+/// \code
+/// float = 0; # entries are allowed outside a section
+///
+/// <section_name>
+/// 	integer = 2;     # Anything after '#' is a comment string for that entry
+/// 	float   = 2.0;
+/// 	float_array = 1.0 2.0 3.0 4.0;
+/// 	string = This is a message;
+/// 	<sub_section_name> # nested subsection
+/// 		float  = 3.0;
+/// 		<sub_sub_section> # 2nd level nesting
+///         </sub_sub_section>
+///     </sub_section_name>
+/// </section_name>
+/// \endcode
+///
+/// <b>Example Program:</b>
+/// \include ConfigFileExample.cpp
+
+class GRAPEUTILS_DLL_API ConfigFile
 {
 public:
     ConfigFile();
+    ~ConfigFile();
 
 private:
 
@@ -26,7 +59,7 @@ private:
     {
     public:
         Grape::Config node;
-        std::map<std::string/*name*/, Config> children;
+        std::map<std::string/*name*/, ConfigNode> children;
     }; // ConfigNode
 
 }; // ConfigFile
